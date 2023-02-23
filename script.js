61fa188af6287eca65820b65f9d5aa73fdb36fdf
dr5w <111304827+dr5w@users.noreply.github.com>
2023-02-23 10:06:07 -0600

    finished exercise

---
+++ script.js
@@ -0,0 +1,98 @@
+// Spread and Destructuring Exercise
+// 1a
+const mcuShows = [
+  `Loki`,
+  `Moon Knight`
+];
+
+// 1b
+const starWarsShows = [
+  "The Mandalorian",
+  "The Book of Boba Fett"
+];
+
+// 1c
+const disneyPlusShows = [
+  ...mcuShows,
+  ...starWarsShows,
+  `The Beatles: Get Back`
+];
+
+// 1d
+console.log(disneyPlusShows);
+/*
+[
+  'Loki',
+  'Moon Knight',
+  'The Mandalorian',
+  'The Book of Boba Fett',
+  'The Beatles: Get Back'
+]
+*/
+
+// 2a
+const netflixMovies = {
+  action: "Extraction",
+  crime: "The Irishman"
+};
+
+// 2b
+const amazonPrimeMovies = {
+  action: "The Tomorrow War",
+  drama: "One Night In Miami"
+};
+
+// 2c
+const streamingMovies = {
+  ...amazonPrimeMovies,
+  ...netflixMovies,
+  musical: "Hamilton"
+};
+
+// 2d
+console.log(streamingMovies);
+/*
+{
+  action: 'Extraction',
+  drama: 'One Night In Miami',
+  crime: 'The Irishman',
+  musical: 'Hamilton'
+}
+*/
+
+// 3a
+const disneyJunior = [
+  "Mickey Mouse Clubhouse",
+  "Spidey and His Amazing Friends"
+];
+
+// 3b
+const [mickey, spidey] = disneyJunior;
+
+// 3c
+console.log(mickey, spidey); // Mickey Mouse Clubhouse Spidey and His Amazing Friends
+
+// 4a
+const avengers = {
+  warMachine: `James Rhodes`,
+  theHulk: `Bruce Banner`
+};
+
+// 4b
+const {warMachine, theHulk} = avengers;
+
+// 4c
+console.log(warMachine, theHulk); // James Rhodes Bruce Banner
+
+// 4d
+const moreAvengers = {
+  blackWidow: `Natasha Romanoff`,
+  hawkeye: `Clint Barton`,
+  ironMan: `Tony Stark`
+};
+
+// 4e
+const {blackWidow: nat, ...others} = moreAvengers;
+
+// 4f
+console.log(nat, others); // Natasha Romanoff { hawkeye: 'Clint Barton', ironMan: 'Tony Stark' }
